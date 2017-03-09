62a2be3bacf42ecfe7c5fc9f16d2b779c97909d9
Christopher Best <bestchris@gmail.com>
2017-03-08 21:30:02 -0500

    name webpack output

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var webpack = require('gulp-webpack');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     return gulp.src('index.js')
-    .pipe(webpack())
+    .pipe(webpack({ output: { filename: 'viz-generator.js' } }))
     .pipe(gulp.dest('dist/'));
 });
