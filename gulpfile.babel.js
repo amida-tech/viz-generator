3589475a428045eba51b4e0f5c998251d9de1345
Christopher Best <bestchris@gmail.com>
2017-03-09 14:09:04 -0500

    move library entry to /src

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,6 @@ import gulp from 'gulp';
 import webpack from 'gulp-webpack';
 import webpackconfig from './webpack.config';
 
-gulp.task('build', () => gulp.src('index.js')
+gulp.task('build', () => gulp.src('src/index.js')
     .pipe(webpack(webpackconfig))
     .pipe(gulp.dest('dist/')));
