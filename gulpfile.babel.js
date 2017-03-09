6eec1dbf4153d4273c5c80f58d0c7e8401b9b220
Christopher Best <bestchris@gmail.com>
2017-03-08 22:02:32 -0500

    fix from eslint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,6 @@
 import gulp from 'gulp';
 import webpack from 'gulp-webpack';
 
-gulp.task('default', function () {
-    return gulp.src('index.js')
+gulp.task('default', () => gulp.src('index.js')
     .pipe(webpack({ output: { filename: 'viz-generator.js' } }))
-    .pipe(gulp.dest('dist/'));
-});
+    .pipe(gulp.dest('dist/')));
