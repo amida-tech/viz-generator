e76ad010dffbbc0319e1d033b59136b06f483d40
Christopher Best <bestchris@gmail.com>
2017-03-09 10:56:01 -0500

    move webpack config to a file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import webpack from 'gulp-webpack';
+import webpackconfig from './webpack.config';
 
 gulp.task('default', () => gulp.src('index.js')
-    .pipe(webpack({ output: { filename: 'viz-generator.js' } }))
+    .pipe(webpack(webpackconfig))
     .pipe(gulp.dest('dist/')));
