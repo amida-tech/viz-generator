f665c2a6c414047c035364ba98fe50e3042842f0
Christopher Best <bestchris@gmail.com>
2017-03-10 10:26:35 -0500

    move spec file to /test

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
         files: [
-            './src/**/*.spec.js',
+            './test/**/*.spec.js',
         ],
 
 
@@ -27,7 +27,7 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            './src/**/*.spec.js': ['webpack'],
+            './test/**/*.spec.js': ['webpack'],
         },
 
         webpack: require('./webpack.config.js'),
