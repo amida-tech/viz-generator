85660c31a25fbad44e4a0c6519fe151f8587ee99
Christopher Best <bestchris@gmail.com>
2017-03-10 11:48:44 -0500

    remove comments from test file

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,9 +2,6 @@ import { hello } from 'index';
 
 describe('viz', () => {
     it('returns \'hello world\'', () => {
-        // console.log(vizGenerator);
-        // expect(vizGenerator).to.exist;
-        // expect(vizGenerator.hello()).to.equal('hello world');
         expect(hello).to.exists;
         expect(hello()).to.equal('hello world');
     });
