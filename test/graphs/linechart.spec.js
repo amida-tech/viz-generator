b77308bb48581cec4bd508d43b71d529cfdffa92
Christopher Best <bestchris@gmail.com>
2017-03-13 15:45:56 -0400

    add line chart data fixture add data and option arg tests

--- test/graphs/linechart.spec.js
+++ test/graphs/linechart.spec.js
@@ -1,9 +1,13 @@
 import { LineChart, makeLineChart } from 'graphs/linechart';
+import data from 'fixtures/chartdata/linechart.json';
 
 describe('LineChart', () => {
     it('exists', () => expect(LineChart).to.exist);
 
     it('throws exception with no data', () => {
-        expect(() => new LineChart()).to.throw(/No data provided/);
+        expect(() => new LineChart()).to.throw('No data provided to LineChart');
     });
+
+    it('throws exception with no options', () =>
+        expect(() => new LineChart(data)).to.throw('No options provided to LineChart'));
 });
