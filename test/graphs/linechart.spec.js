1bd07e4a7b82ff53b15fc763976a7567df5ee457
Christopher Best <bestchris@gmail.com>
2017-03-13 14:29:31 -0400

    add existence check

--- test/graphs/linechart.spec.js
+++ test/graphs/linechart.spec.js
@@ -1,6 +1,8 @@
 import { LineChart, makeLineChart } from 'graphs/linechart';
 
 describe('LineChart', () => {
+    it('exists', () => expect(LineChart).to.exist);
+
     it('throws exception with no data', () => {
         expect(() => new LineChart()).to.throw(/No data provided/);
     });
