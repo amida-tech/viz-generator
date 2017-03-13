4a0743bcc92f7ea41e84299209dba9d27d051a1e
Christopher Best <bestchris@gmail.com>
2017-03-13 15:53:38 -0400

    implement Graph constructor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-export class Graph { }
+export class Graph {
+    constructor(data, options) {
+        this.data = data;
+        this.options = options;
+    }
+
+    render(node) {
+    }
+}
 
 export function hello() {
     return 'hello world';
