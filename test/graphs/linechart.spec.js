df1bb94ab5d7f8322f4c3a8395e569388c9bcd5e
Christopher Best <bestchris@gmail.com>
2017-03-13 21:19:39 -0400

    add data and options linechart tests

--- test/graphs/linechart.spec.js
+++ test/graphs/linechart.spec.js
@@ -1,13 +1,61 @@
 import { LineChart, makeLineChart } from 'graphs/linechart';
-import data from 'fixtures/chartdata/linechart.json';
+import dataFixture from 'fixtures/chartdata/linechart.json';
+import optionsFixture from 'fixtures/chartoptions/linechart.json';
 
 describe('LineChart', () => {
     it('exists', () => expect(LineChart).to.exist);
 
-    it('throws exception with no data', () => {
+    let data,
+        options;
+    beforeEach(() => {
+        data = JSON.parse(JSON.stringify(dataFixture));
+        options = JSON.parse(JSON.stringify(optionsFixture));
+    });
+
+    it('throws an exception with no data', () => {
         expect(() => new LineChart()).to.throw('No data provided to LineChart');
     });
 
-    it('throws exception with no options', () =>
+    it('throws an exception with no options', () =>
         expect(() => new LineChart(data)).to.throw('No options provided to LineChart'));
+
+    it('throws an exception with no plot parameters', () => {
+        delete options.plotParams;
+        expect(() => new LineChart(data, options)).to.throw('No plot parameters provided to LineChart');
+    });
+
+    it('throws an exception with no x-axis specified', () => {
+        delete options.plotParams.x;
+        expect(() => new LineChart(data, options)).to.throw('No x axis specified in LineChart options');
+    });
+
+    it('throws an exception with no y-axis specified', () => {
+        delete options.plotParams.y;
+        expect(() => new LineChart(data, options)).to.throw('No y axis specified in LineChart options');
+    });
+
+    it('throws an exception with no data headers specified', () => {
+        delete data.header;
+        expect(() => new LineChart(data, options)).to.throw('No header specified in LineChart data');
+    });
+
+    it('throws an exception with no x-axis header', () => {
+        delete data.header.values[0][options.plotParams.x];
+        expect(() => new LineChart(data, options)).to.throw(`No header value specified for x axis (${options.plotParams.x})`);
+    });
+
+    it('throws an exception with no x-axis display name', () => {
+        delete data.header.values[0][options.plotParams.x].display_name;
+        expect(() => new LineChart(data, options)).to.throw(`No header value specified for x axis (${options.plotParams.x})`);
+    });
+
+    it('throws an exception with no y-axis header', () => {
+        delete data.header.values[0][options.plotParams.y];
+        expect(() => new LineChart(data, options)).to.throw(`No header value specified for y axis (${options.plotParams.y})`);
+    });
+
+    it('throws an exception with no y-axis display name', () => {
+        delete data.header.values[0][options.plotParams.y].display_name;
+        expect(() => new LineChart(data, options)).to.throw(`No header value specified for y axis (${options.plotParams.y})`);
+    });
 });
