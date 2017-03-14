699f46d071b33d06681f9c2875bd9bf926b3d127
Christopher Best <bestchris@gmail.com>
2017-03-13 21:20:21 -0400

    add data and option validation in LineChart constructor

--- src/graphs/linechart.js
+++ src/graphs/linechart.js
@@ -12,6 +12,21 @@ export class LineChart extends Graph {
         if (!this.data) return 'No data provided to LineChart';
         if (!this.options) return 'No options provided to LineChart';
 
+        // option validation
+        if (!this.options.plotParams) return 'No plot parameters provided to LineChart';
+        if (!this.options.plotParams.x) return 'No x axis specified in LineChart options';
+        if (!this.options.plotParams.y) return 'No y axis specified in LineChart options';
+
+        // heder validation
+        if (!this.data.header) return 'No header specified in LineChart data';
+        if (!this.data.header.values[0][this.options.plotParams.x] ||
+            !this.data.header.values[0][this.options.plotParams.x].display_name) {
+            return `No header value specified for x axis (${this.options.plotParams.x})`;
+        }
+        if (!this.data.header.values[0][this.options.plotParams.y] ||
+            !this.data.header.values[0][this.options.plotParams.y].display_name) {
+            return `No header value specified for y axis (${this.options.plotParams.y})`;
+        }
         return null;
     }
 }
