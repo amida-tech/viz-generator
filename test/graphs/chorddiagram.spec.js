2b11877df81b817c67581e28979e22e7378270d5
Christopher Best <bestchris@gmail.com>
2017-03-20 15:51:09 -0400

    start chord diagram tests

---
+++ test/graphs/chorddiagram.spec.js
@@ -0,0 +1,42 @@
+import { ChordDiagram, makeChordDiagram } from 'graphs/chorddiagram';
+import dataFixture from 'fixtures/chartdata/chorddiagram.json';
+import optionsFixture from 'fixtures/chartoptions/chorddiagram.json';
+import { expect } from 'chai';
+
+describe('ChordDiagram', () => {
+    it('exists', () => expect(ChordDiagram).to.exist);
+
+    let data;
+    let options;
+    beforeEach(() => {
+        data = JSON.parse(JSON.stringify(dataFixture));
+        options = JSON.parse(JSON.stringify(optionsFixture));
+
+        // add chart div to install into
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+        const chartDiv = document.createElement('div');
+        chartDiv.id = 'chart';
+        document.body.append(chartDiv);
+    });
+
+    it('throws an exception with no data', () => {
+        expect(() => new ChordDiagram()).to.throw('No data provided to ChordDiagram');
+    });
+
+    it('throws an exception with no options', () =>
+        expect(() => new ChordDiagram(data)).to.throw('No options provided to ChordDiagram'));
+
+    it('throws an exception with no plot parameters', () => {
+        delete options.plotParams;
+        expect(() => new ChordDiagram(data, options)).to.throw('No plot parameters provided to ChordDiagram');
+    });
+
+    it('renders svg into the dom', (done) => {
+        new ChordDiagram(data, options).render(document.querySelector('div')).then(() => {
+            expect(document.querySelector('svg')).to.be.not.null;
+            done();
+        });
+    });
+});
