85b71f2a2cf94a9723726bccb0d7988b2b76066a
Christopher Best <bestchris@gmail.com>
2017-03-20 15:32:54 -0400

    resolve eslint issues in root tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,10 @@
 import * as viz from 'index';
+import { expect } from 'chai';
 
 describe('viz', () => {
     it('defines superclass', () => expect(viz.Graph).to.exist);
 
     it('says hi', () => {
-        expect(viz.hello).to.exist;
         expect(viz.hello()).to.equal('hello world');
     });
 });
